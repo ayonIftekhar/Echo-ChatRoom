a8dc2b865c4aeb77061a218dedabd74b596f97d2
ayonIftekhar <ayon21.official@gmail.com>
2025-04-25 04:18:51 +0600

    under dev 1.29

--- EchoRoomFrontEnd/ECHO_ROOM/src/pages/LogOut/Logout.jsx
+++ EchoRoomFrontEnd/ECHO_ROOM/src/pages/LogOut/Logout.jsx
@@ -2,6 +2,8 @@ import { useNavigate } from "react-router-dom";
 import { logOutFromServer } from "../../BackendAPI/UserBackend";
 import { toast } from "react-toastify";
 import { useEffect } from "react";
+import assets from "../../assets/assets";
+import { Spinner } from "react-bootstrap";
 
 export function LogOut(){
     const navigate = useNavigate();
@@ -22,9 +24,23 @@ export function LogOut(){
         signOut();
     },[])
 
-    return(
-        <div>
-            <h1>Logging out...</h1>
+    return (
+      <div className="vh-100 d-flex flex-column justify-content-center align-items-center bg-light">
+        <div className="text-center">
+          <img
+            src={assets.logo}
+            alt="EchoRoom Logo"
+            style={{ width: "100px", marginBottom: "20px" }}
+          />
+
+          <h2 className="fw-bold mb-3 text-danger">Logging you out...</h2>
+
+          <Spinner animation="border" variant="danger" role="status" />
+
+          <p className="text-muted mt-3">
+            Thank for joining <strong className="text-primary">EchoRoom</strong> community.
+          </p>
         </div>
-    )
+      </div>
+    );
 }
