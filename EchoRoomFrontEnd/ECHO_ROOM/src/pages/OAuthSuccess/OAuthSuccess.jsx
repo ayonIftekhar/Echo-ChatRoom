bb71f2c881abe0c8f34ffd97bbe699f92bf5bf9e
ayonIftekhar <ayon21.official@gmail.com>
2025-04-24 19:52:18 +0600

    under dev 1.19

--- EchoRoomFrontEnd/ECHO_ROOM/src/pages/OAuthSuccess/OAuthSuccess.jsx
+++ EchoRoomFrontEnd/ECHO_ROOM/src/pages/OAuthSuccess/OAuthSuccess.jsx
@@ -1,10 +1,26 @@
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { UserRoomContext } from "../../Context/RoomContext";
+import axios from "axios";
+import { toast } from "react-toastify";
+    
 export default function OAuthSuccess() {
     const navigate = useNavigate();
 
+    const { setUser } = useContext(UserRoomContext);
+    const backendURL = import.meta.env.VITE_API_BASE_URL;
+
     useEffect(() => {
-        navigate("/");
+      axios
+        .get(`${backendURL}/api/me`, { withCredentials: true })
+        .then((res) => {
+          setUser(res.data);
+          navigate("/");
+        })
+        .catch(() => {
+          toast.error("OAuth login failed");
+          navigate("/login");
+        });
     }, []);
 
     return (
