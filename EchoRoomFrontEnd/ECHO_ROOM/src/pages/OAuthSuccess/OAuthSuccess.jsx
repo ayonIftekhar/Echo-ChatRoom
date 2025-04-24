5e5ad3ebb6e5511e80b94d64140b2fd45eb5e848
ayonIftekhar <ayon21.official@gmail.com>
2025-04-25 04:10:54 +0600

    under dev 1.28

--- EchoRoomFrontEnd/ECHO_ROOM/src/pages/OAuthSuccess/OAuthSuccess.jsx
+++ EchoRoomFrontEnd/ECHO_ROOM/src/pages/OAuthSuccess/OAuthSuccess.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { UserRoomContext } from "../../Context/RoomContext";
 import axios from "axios";
 import { Spinner } from "react-bootstrap";
-import assets from "../../assets/assets";
+
 
 export default function OAuthSuccess() {
     const navigate = useNavigate();
@@ -34,11 +34,7 @@ export default function OAuthSuccess() {
     return (
       <div className="vh-100 d-flex flex-column justify-content-center align-items-center bg-light">
         <div className="text-center">
-          <img
-            src={assets.logo} // optional logo
-            alt="EchoRoom Logo"
-            style={{ width: "100px", marginBottom: "20px" }}
-          />
+          
 
           <h2 className="fw-bold mb-3 text-primary">Logging you in...</h2>
 
