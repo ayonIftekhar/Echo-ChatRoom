2b7557ea45ce09650f330d0aa7c9e90e13a61b1d
ayonIftekhar <ayon21.official@gmail.com>
2025-04-24 20:24:54 +0600

    under dev 1.21

--- EchoRoomFrontEnd/ECHO_ROOM/src/pages/OAuthSuccess/OAuthSuccess.jsx
+++ EchoRoomFrontEnd/ECHO_ROOM/src/pages/OAuthSuccess/OAuthSuccess.jsx
@@ -2,25 +2,28 @@ import { useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { UserRoomContext } from "../../Context/RoomContext";
 import axios from "axios";
-import { toast } from "react-toastify";
-    
+
 export default function OAuthSuccess() {
     const navigate = useNavigate();
 
     const { setUser } = useContext(UserRoomContext);
     const backendURL = import.meta.env.VITE_API_BASE_URL;
 
-    useEffect(() => {
-      axios
-        .get(`${backendURL}/api/me`, { withCredentials: true })
-        .then((res) => {
-          setUser(res.data);
-          navigate("/");
-        })
-        .catch(() => {
-          toast.error("OAuth login failed");
-          navigate("/login");
+    async function getUser() {
+      try {
+        const res = await axios.get(`${backendURL}/api/me`, {
+          withCredentials: true,
         });
+        setUser(res.data);
+      } catch (err) {
+        navigate("/login");
+        console.log("success handler er moddhe error ase");
+        console.log(err);
+      }
+    }
+
+    useEffect(() => {
+      getUser();
     }, []);
 
     return (
